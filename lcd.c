45663c16a26d4b204305803dcf1ff061ac6b1a09
aranajad <111802980+aranajad@users.noreply.github.com>
2023-11-21 11:45:12 -0500

    added LCDWrite2

--- lcd.c
+++ lcd.c
@@ -23,24 +23,30 @@ void GPIOA_Init();
 void LCD_Enable();
 void LCD_Init();
 void LCD_Clear();
-void LCD_Write();
+void LCD_Write(int, int,int,int,int,int,int,int);
+void LCD_Write(int);
 void LCD_Display(unsigned char D, unsigned char C, unsigned char B);
 
 int main(void){
 	GPIOA_Init();
 	LCD_Init();
 	LCD_Write(0,1,0,0,1,0,0,0); //Write decimal 72 (H)
-	LCD_Write(0,1,0,0,1,0,0,0,1); //Write decimal 73(I)
+	LCD_Write(0,1,0,0,1,0,0,1); //Write decimal 73(I)
 	while(1){}
 }
 
+/* Enables Pins 0 - 16 as O/P
+ * Control Sgnals for the LCD are PA_2, PA_1, PA_0 (RS, R/W, ENABLE)
+ * D7-D4 are PA_7-PA_4
+ */
 void GPIOA_Init(){
 	// Enable GPIOA peripheral clock
 	*RCC_AHB2ENR |= 1; // *RCC_AHB2ENR = *RCC_AHB2ENR | (1<<0);
 	// Set GPIOA PA9-0 as output
-	*GPIOA_MODER = (*GPIOA_MODER & ~(0xFFFFF)) | 0x55555;
+	*GPIOA_MODER = (*GPIOA_MODER & ~(0xFFFFF)) | 0x5555;
 }
 
+//Assumes RS is PA_9, R/W Select is PA_8, and d7-d4 are PA_7 to PA_4
 void LCD_Init(){
 	//_____Initilize the LCD to 4 bit length, 2 line, 5x8 Font_________________
 	//OPCODE = 0b00001(DL)(N)(F)XX; DL = 0 (4 bits), N = 1 (2 lines), F = 0 (5x8)
@@ -48,6 +54,12 @@ void LCD_Init(){
 	//Must send OPCODE 4 bits at a time
 	*GPIOA_ODR &= ~(1<<9|1<<8); //RS & R/W = 0
 
+	//D7-D4 = 0010
+	*GPIOA_ODR &= ~(1<<7|1<<6|1<<4);
+	*GPIOA_ODR |= 1<<5;
+	
+	LCD_Enable();
+	
 	//D7-D4 = 0010
 	*GPIOA_ODR &= ~(1<<7|1<<6|1<<4);
 	*GPIOA_ODR |= 1<<5;
@@ -107,14 +119,15 @@ void LCD_Enable(){
 	//Set PA_0 = 1
 	*GPIOA_ODR |= 1; 
 
-	//Busy wait
-	for(int i = 0; i < 4;i++){}
+	//Busy wait (1mS)
+	for(int i = 0; i < 4003;i++){}
 
 	//Set PA_0 = 0
 	*GPIOA_ODR &= ~1; 
 	
 }
 
+//Assumes RS is PA_9, R/W Select is PA_8, and d7-d4 are PA_7 to PA_4
 void LCD_Clear(){
 	//Clear the display (Write 0b0000000001)
 	volatile unsigned int* GPIOA_ODR = GPIOA_MODER + 0x14;
@@ -129,6 +142,7 @@ void LCD_Clear(){
  * l7-l4 are the lower 4 bits set for the OPCODE
  * 
  * OPCODE = 10 | (d7-d4) | (l7-l4)
+ * Assumes RS is PA_9, R/W Select is PA_8, and d7-d4 are PA_7 to PA_4
  */
 void LCD_Write(int d7, int d6, int d5, int d4, int l7, int l6, int l5, int l4){
 	//OPCODE = 10 BBBB BBBB
@@ -138,6 +152,83 @@ void LCD_Write(int d7, int d6, int d5, int d4, int l7, int l6, int l5, int l4){
 	*GPIOA_ODR &= ~(1<<8);
 
 	//D7-D4 = d7-d4
+
+	//if d7 = 1 then set 7th bit in ODR to 1 else set it to 0
+	if(d7){
+		*GPIOA_ODR |= (d7<<7);
+	} else {
+		*GPIOA_ODR &= ~(1<<7);
+	}
+
+	if(d6){
+		*GPIOA_ODR |= (d6<<6);
+	} else {
+		*GPIOA_ODR &= ~(1<<6);
+	}
+
+	if(d5){
+		*GPIOA_ODR |= (d5<<5);
+	} else {
+		*GPIOA_ODR &= ~(1<<5);
+	}
+
+	if(d4){
+		*GPIOA_ODR |= (d4<<4);
+	} else {
+		*GPIOA_ODR &= ~(1<<4);
+	}
+
+	LCD_Enable();
+
+	//D7-D4 = l7-l4
+	if(l7){
+		*GPIOA_ODR |= (l7<<7);
+	} else {
+		*GPIOA_ODR &= ~(1<<7);
+	}
+
+	if(l6){
+		*GPIOA_ODR |= (l6<<6);
+	} else {
+		*GPIOA_ODR &= ~(1<<6);
+	}
+
+	if(l5){
+		*GPIOA_ODR |= (l5<<5);
+	} else {
+		*GPIOA_ODR &= ~(1<<5);
+	}
+
+	if(l4){
+		*GPIOA_ODR |= (l4<<4);
+	} else {
+		*GPIOA_ODR &= ~(1<<4);
+	}
+
+	LCD_Enable();
+}
+
+/* Generates an OPCODE where:
+ * RS = 1, R/W = 0
+ * d7-d4 are the upper 4 bits of the ASCII value
+ * l7-l4 are the lower 4 bits of the ASCII value
+ * 
+ * OPCODE = 10 | (d7-d4) | (l7-l4)
+ * Assumes RS is PA_9, R/W Select is PA_8, and d7-d4 are PA_7 to PA_4
+ */
+void LCD_Write(unsigned int asc){
+	//OPCODE = 10 BBBB BBBB
+	//To get upper 4 bits do c >> 4
+	//To get lower 4 bits do c & 0XF
+	//To enter into ODR move RS & R/W are in PA_3 and PA_2 
+
+	//[RS, R/W] = 10
+	*GPIOA_ODR |= (1<<9);
+	*GPIOA_ODR &= ~(1<<8);
+
+	//D7-D4 = d7-d4
+	*GPIOA_ODR &= ~(1<<7|1<<6|1<<5|1<<4); //0s out D7-D4
+	*GPIOA_ODR |= ; //0s out D7-D4
 
 	//if d7 = 1 then set 7th bit in ODR to 1 else set it to 0
 	if(d7){
