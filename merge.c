e739f159a7f063920ca6fd58cf91b871385c7928
aranajad <111802980+aranajad@users.noreply.github.com>
2023-12-04 10:45:22 -0500

    partial implementation of PWM3/2 for PB_5 (Buzzer)

--- merge.c
+++ merge.c
@@ -4,6 +4,7 @@
 #define OFF (unsigned char) 0
 
 void TIM2_Init(void);
+void TIM3_Init(void);
 void TIM2_IRQHandler(void);
 void TIM2_Enable(unsigned char a);
 extern void GPIO_Init();
@@ -20,6 +21,7 @@ extern void LCD_Display(unsigned char D, unsigned char C, unsigned char B);
 extern void print_time(unsigned int time);
 extern void print_mode(int mode);
 extern int setMode();
+void playBuzzer();
 static unsigned int mode = 1;
 static unsigned int start = 0;
 
@@ -173,6 +175,9 @@ void TIM2_IRQHandler()
 		return;
 }
 
+/* Passing ON (1) -> TIM2 is Enabled
+ * Passing OFF (0) -> TIM2 is Disabled
+ */
 void TIM2_Enable(unsigned char a){
 	volatile unsigned int* TIM2_CR1 = (unsigned int*) 0x40000000;	
     if(a){
@@ -180,4 +185,98 @@ void TIM2_Enable(unsigned char a){
     } else{
         *TIM2_CR1 &= (a << 0);
     }
+}
+
+/* Generates are square wave (50% duty cycle PWM) on TIM3 Channel 2 (PWM3/2)
+ * PWM3/2 maps to D4 (PB_5)
+ * Attached to a buzzer on the board. Frequency of square wave determines pitch of buzzer
+ */
+void playBuzzer(){
+}
+
+void TIM3_Init(unsigned int desired_freq){
+	
+	volatile unsigned int* RCC_AHB2ENR = (unsigned int*) 0x4002104C;
+	volatile unsigned int* RCC_APB1ENR1 = (unsigned int*) 0x40021058;
+	volatile unsigned int* TIM3_ARR = (unsigned int*) 0x4000042C;
+	volatile unsigned int* TIM3_CR1 = (unsigned int*) 0x40000400;	
+	volatile unsigned int* TIM3_PSC = (unsigned int*) 0x40000428;	
+	volatile unsigned int* TIM3_CNT = (unsigned int*) 0x40000424;	
+	volatile unsigned int* TIM3_DIER = (unsigned int*) 0x4000040C;	
+	volatile unsigned int* NVIC_ISER0 = (unsigned int*) 0xE000E100;	
+	//void PWM_Init(unsigned int duty, unsigned int period)
+	volatile unsigned int* TIM3_CCR1 = (unsigned int*) 0x40000434;
+	volatile unsigned int* GPIOB_MODER = (unsigned int*) 0x48000400; //GPIOB, using PB3: Tim2-CH2
+	volatile unsigned int* GPIOB_AFRL = (unsigned int*) 0x48000420;		//AF1; dont worry using this, for pwm	
+	volatile unsigned int* TIM3_CCMR1 = (unsigned int*) 0x40000418;
+	volatile unsigned int* TIM3_CCER = (unsigned int*) 0x40000420;	same with all these above
+	
+	// Enable GPIOB peripheral clock
+	*RCC_AHB2ENR |= (0x01);
+	
+	//***********
+	// Configure PB_5 to alt. function mode
+	*GPIOB_MODER &= ~(0b11 << 10); //Zero out MODE5
+	*GPIOB_MODER |=  (0b10 << 10); //Mode 5 = 10 (Alt Func Mode)
+
+	*GPIOB_AFRL &= ~(0b0010 << 20); //Zero AFSEL5 (AF2 = TIM3/2)
+	*GPIOB_AFRL |=  (0b0010 << 20); //Set AFSEL5 to 0010 (AF2 = TIM3/2)
+
+	//Enable OC1PE bit
+	*TIM3_CCMR1 |= (1<<3);
+
+	//Set ARR to upcounting via ARPE in TIM2_CR1
+	*TIM3_CR1 |= (1<<7);
+
+	//Generate PWM via CMS bits in TIM2_CR1
+	*TIM3_CR1 &= ~(1<<6 | 1<<5);
+
+	//1 for Upcount & 0 for downcount (DIR bit)
+	*TIM3_CR1 &= ~(1<<4); 
+
+	//Set OC1 Polarity w/ CC1P bit
+	*TIM3_CCER &= ~(1<<1);
+
+	// Enable TIM3 peripheral clock
+	*RCC_APB1ENR1 |= 1<<1;
+
+	//set PSC if needed (0 by default)
+	*TIM3_PSC = 0;
+
+	unsigned int calcARR = (unsigned int) ((4e6)/desired_freq)-1;
+	unsigned int calcDuty = (unsigned int)calcARR / 2;
+
+	//EQN for determining ARR
+	//ARR = (CLK_Freq/Desire_Freq) - 1
+	//ARR = (4e6/Desired_Freq)-1
+	//TIM3_ARR = (unsigned int) ((4e6)/[something])- 1; //261 is a Middle C
+	*TIM3_ARR = calcARR;
+
+	// Set duty cycle
+	*TIM3_CCR1 = calcDuty;
+	
+	//CCMR1 stuff goes above CCER stuff
+
+	//Enable output for channel 2
+	*TIM3_CCER |= 1<<4; //Sets CC2E (Capture/Computer 2 Enable) to 1
+
+//*****************************************UNFINISHED*********************************************
+//Still need to up the code for everything below this to work properly
+	
+	//***********************MAKE SURE YOU ENABLE OUTPUT FOR CHANNEL 2 NOT 1.
+	// Select PWM mode 1 on channel 1 (OC1)
+	//*TIM3_CCMR1 &= ~(1 << 16);
+	//*TIM3_CCMR1 = (*TIM2_CCMR1 | (3 << 5)) & ~(1 << 4 ); FOR PWM NOT NEEDED
+
+
+	//enable up-counting mode: clr tim2_cr1(4) AND enable clock tim2_crl(0);
+	*TIM3_CR1 |= (1 << 0);
+	
+	//Enable capture/compare and update interrupts: TIM2_DIER(1:0) = "11"
+	*TIM3_DIER = 3;
+	
+	//Enable TIM2 interrupts in NVIC: NVIC
+	*NVIC_ISER0 |= (1 << 28);
+	
+	return;
 }
